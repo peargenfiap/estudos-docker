3fbbb3d800fe6f45abdf1202f1d3f54bb2dcadf2
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-24 22:42:47 -0300

    adicionado barramento de fila

---
+++ Fiap.Cartao/Fiap.Cartao/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Fiap.Cartao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+            Console.WriteLine(" [*] Aguardando novas mensagens.");
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] recebido {message}");
+
+                channel.BasicAck(ea.DeliveryTag, false);
+            };
+            channel.BasicConsume(queue: "hello",
+                                 autoAck: false,
+                                 consumer: consumer);
+            Console.WriteLine(" Pressione [enter] para finalizar.");
+            Console.ReadLine();
+
+
+        }
+    }
+}
